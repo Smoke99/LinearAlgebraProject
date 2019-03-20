190f8d55782e8037f8317f88da9c88fbd2a204e0
stefa <stefansmoke@gmail.com>
2019-03-20 16:07:36 -0400

    ~Initial Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,56 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+
+    @FXML
+    private Button CalcButton;
+
+    @FXML
+    private TextField IP2;
+
+    @FXML
+    private TextField IP1;
+
+    @FXML
+    private TextField IP4;
+
+    @FXML
+    private TextField IP3;
+
+    @FXML
+    private TextField Result;
+
+    @FXML
+    void StripIP(ActionEvent event) {
+        String ipone = IP1.getText();
+        char[] IPOne = ipone.toCharArray();
+        System.out.println(ipone);
+
+        String iptwo = IP2.getText();
+        char[] IPTwo = iptwo.toCharArray();
+        System.out.println(iptwo);
+
+        String ipthree = IP3.getText();
+        char[] IPThree = ipthree.toCharArray();
+
+        String ipfour = IP4.getText();
+        char[] IPFour = ipfour.toCharArray();
+
+        String s = ipone + " " + iptwo;
+
+        PrintText(s);
+    }
+
+    void PrintText(String s)
+    {
+        Result.setText(s);
+    }
+
+
+}
