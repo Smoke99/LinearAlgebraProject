be47ba22274c4cc0a56eb567a2a896320c5d4eeb
stefa <stefansmoke@gmail.com>
2019-04-09 16:10:58 -0400

    ~Initial Commit

--- src/sample/WIndowController.java
+++ src/sample/WIndowController.java
@@ -35,6 +35,9 @@ public class WIndowController {
     private TextField N2ToN4;
 
     @FXML
+    private TextField Output1;
+
+    @FXML
     private TextField Output2;
 
     @FXML
@@ -45,8 +48,49 @@ public class WIndowController {
 
     @FXML
     void Calculate(ActionEvent event) {
+        int I1 = Integer.parseInt(Input1.getText());
+        int I2 = Integer.parseInt(Input2.getText());
+        int x = Integer.parseInt(N1ToN2.getText());
+        int y = Integer.parseInt(N1ToN3.getText());
+        int z = Integer.parseInt(N3ToN4.getText());
+        int w = Integer.parseInt(N2ToN4.getText());
+        int O1 = Integer.parseInt(Output1.getText());
+        int O2 = Integer.parseInt(Output2.getText());
 
-        
+        int[][] matrix = new int[4][5];
+        matrix[0][0] = 0;
+        matrix[0][1] = x;
+        matrix[0][2] = y;
+        matrix[0][3] = 0;
+        matrix[0][4] = I1;
+
+        matrix[1][0] = 0;
+        matrix[1][1] = 0;
+        matrix[1][2] = -y;
+        matrix[1][3] = z;
+        matrix[1][4] = I2;
+
+        matrix[2][0] = -w;
+        matrix[2][1] = x;
+        matrix[2][2] = 0;
+        matrix[2][3] = 0;
+        matrix[2][4] = O1;
+
+        matrix[3][0] = w;
+        matrix[3][1] = 0;
+        matrix[3][2] = 0;
+        matrix[3][3] = z;
+        matrix[3][4] = O2;
+
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+
 
     }
 
@@ -58,6 +102,7 @@ public class WIndowController {
         assert N3ToN4 != null : "fx:id=\"N3ToN4\" was not injected: check your FXML file 'Window.fxml'.";
         assert N1ToN3 != null : "fx:id=\"N1ToN3\" was not injected: check your FXML file 'Window.fxml'.";
         assert N2ToN4 != null : "fx:id=\"N2ToN4\" was not injected: check your FXML file 'Window.fxml'.";
+        assert Output1 != null : "fx:id=\"Output1\" was not injected: check your FXML file 'Window.fxml'.";
         assert Output2 != null : "fx:id=\"Output2\" was not injected: check your FXML file 'Window.fxml'.";
         assert Input2 != null : "fx:id=\"Input2\" was not injected: check your FXML file 'Window.fxml'.";
         assert Pane != null : "fx:id=\"Pane\" was not injected: check your FXML file 'Window.fxml'.";
